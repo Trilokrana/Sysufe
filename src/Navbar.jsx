493ee0587961ee408a20c8112c94d3a3453a20eb
Trilok Rana <ranatrilok1234@gmail.com>
2024-05-10 17:44:38 +0530

    add one heading

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar() {
         <h1 className="story-header">SYSU</h1>
         <h4>Journal in Public</h4>
         <h3>what next in our plan </h3>
+        <h4>we are moving to the new Category</h4>
       </div>
     </div>
   );
